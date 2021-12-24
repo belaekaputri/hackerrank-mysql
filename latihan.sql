55f27a166b8cb1bb43c88ae09ba3e3d06b986735
belaekaputri <belaekaputri28@gmail.com>
2021-12-24 12:49:35 +0700

    commit kedua

--- latihan.sql
+++ latihan.sql
@@ -30,3 +30,15 @@ SELECT DISTINCT(CITY) FROM STATION WHERE RIGHT(CITY,1) NOT IN ('a','i','u','e','
 SELECT DISTINCT(CITY) FROM STATION WHERE LEFT(CITY,1) NOT IN ('a','i','u','e','o') or RIGHT(CITY,1) NOT IN ('a','i','u','e','o')
 -- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
 SELECT DISTINCT(CITY) FROM STATION WHERE LEFT(CITY,1) NOT IN ('a','i','u','e','o') and right(city,1) not in ('a','i','u','e','o')
+--Query the Name of any student in STUDENTS who scored higher than 75  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+SELECT NAME FROM STUDENTS WHERE MARKS > 75 ORDER BY RIGHT(NAME,3), ID ASC
+--Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+SELECT NAME FROM EMPLOYEE ORDER BY NAME ASC
+--Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than $2000 per month who have been employees for less than 10 months. Sort your result by ascending employee_id.
+SELECT NAME FROM EMPLOYEE WHERE SALARY > 2000 AND MONTHS < 10 ORDER BY EMPLOYEE_ID ASC
+--Query a count of the number of cities in CITY having a Population larger than 100000
+SELECT COUNT(NAME) FROM CITY WHERE POPULATION > 100000
+--Query the total population of all cities in CITY where District is California.
+SELECT SUM(POPULATION) FROM CITY WHERE DISTRICT="California"
+--Query the average population of all cities in CITY where District is California.
+SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT="California"
